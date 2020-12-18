9250227d68d97d06239f7d2d8b18d3f4f74fd091
karimsr4 <33129913+karimsr4@users.noreply.github.com>
2020-12-18 13:52:42 +0100

    corrections

--- plotter.py
+++ plotter.py
@@ -5,34 +5,37 @@ import pandas as pd
 # This function can be used to fit a linear regression model and to plot the segmented regression 
 # The inputs shift_intervention and shift_end_month must be a the intervention month index and end month index relative to the 
 # start month
-def plotter(data, shift_intervention, shift_end_month):
+def plotter(data, graph_title=None):
+    
+    shift_intervention = data[data.intervention==1]["time"].iloc[0]
+    shift_end_month = data.iloc[len(data)-1]['time']
     
     #fitting regression model
-    model =smf.ols('pageviews ~ time + C(intervention) + post_slope', data=data)
+    model = smf.ols('pageviews ~ time + C(intervention) + post_slope', data=data)
     res = model.fit()
     
     #print the summary of the regression model
     print(res.summary())
     
     #retrieving the coefficients of each parameter
-    coeffs= res.params.values
-    time_coef= coeffs[2]
-    intervention_coef= coeffs[1]
-    post_slope_coef= coeffs[3]
-    intercept= coeffs[0]
+    coeffs = res.params.values
+    time_coef = coeffs[2]
+    intervention_coef = coeffs[1]
+    post_slope_coef = coeffs[3]
+    intercept = coeffs[0]
     
     #computing the limits of the regression line for the pre intervention period
-    views_pre_int_first_month= intercept + time_coef * 1
-    views_pre_int_last_month= intercept + time_coef * (shift_intervention-1)
+    views_pre_int_first_month = intercept + time_coef * 1
+    views_pre_int_last_month = intercept + time_coef * (shift_intervention-1)
     
     #computing the limits of the regression line for the post intervention period
-    post_slope_first_month_post= data.set_index("time").loc[shift_intervention, "post_slope"]
-    post_slope_last_month_post= data.set_index("time").loc[shift_end_month, "post_slope"]
-    views_post_int_first_month= intercept + time_coef * shift_intervention + intervention_coef + post_slope_coef * post_slope_first_month_post  
-    views_post_int_last_month= intercept + time_coef * shift_end_month + intervention_coef + post_slope_coef * post_slope_last_month_post   
+    post_slope_first_month_post = data.set_index("time").loc[shift_intervention, "post_slope"]
+    post_slope_last_month_post = data.set_index("time").loc[shift_end_month, "post_slope"]
+    views_post_int_first_month = intercept + time_coef * shift_intervention + intervention_coef + post_slope_coef * post_slope_first_month_post  
+    views_post_int_last_month = intercept + time_coef * shift_end_month + intervention_coef + post_slope_coef * post_slope_last_month_post   
     
     #plotting the two regression lines and data points
-    fig= plt.figure(figsize=(14,9))
+    fig = plt.figure(figsize=(14,9))
     plt.scatter(x="time", y="pageviews", data=data,figure=fig, color="black")
     plt.plot([1,shift_intervention-1], [views_pre_int_first_month, views_pre_int_last_month], figure=fig, color="darkgreen")
     plt.plot([shift_intervention,shift_end_month], [views_post_int_first_month, views_post_int_last_month], figure=fig, color="lightgreen")
